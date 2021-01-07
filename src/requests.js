b7064a1497910200947803e01d3bef86ba62240a
Tesfa <tesfayeykh8@gmail.com>
2021-01-06 23:13:16 -0500

    update

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "7b11cba6b270fc3a557f792fbf168b4d";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRatedMovies: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+export default requests;
+
+
+// 7abc99ee77e8f993d57232eb99399ce8
